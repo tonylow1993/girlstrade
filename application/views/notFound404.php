4e0873e20ff4942829698f7abd8f50d446c47da2
Ryan Fung <ryanfung@gmail.com>
2016-02-16 01:33:56 +0800

    In Progress

---
+++ application/views/notFound404.php
@@ -0,0 +1,10 @@
+<?php $title = "GirlsTrade - Page Not Found";  include("header.php"); ?>
+
+
+<div id="wrapper" style="background:rgb(238, 238, 238);">
+   <div class="main-container" style="padding: 10px 0; padding-bottom:  50px;">
+   
+</div>
+</div>
+<?php include "footer1.php"; ?>
+<?php include "footer2.php"; ?>
