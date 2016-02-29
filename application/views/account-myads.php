d2484a57789374d4aa26490ef4280d1e8a437faf
Raymond Chiu <rchiu5hk@yahoo.com.hk>
2016-02-29 21:09:44 +0800

    amend layout

--- application/views/account-myads.php
+++ application/views/account-myads.php
@@ -30,12 +30,9 @@
               <table id="addManageTable" class="table table-striped table-bordered add-manage-table table demo" data-filter="#filter" data-filter-text-only="true" >
                 <thead>
                   <tr style="height:50px;">
-<!--                <th height="50px" data-type="numeric" data-sort-initial="true" style="border: none;"> </th>-->
-                    <th style="border: none;"> <?php echo $this->lang->line("Photo");?> </th>
+<                   <th style="border: none;"> <?php echo $this->lang->line("Photo");?> </th>
                     <th data-sort-ignore="true" style="border: none;"> <?php echo $this->lang->line("Ads_Detail");?> </th>
-                    <th class="price-td" data-type="numeric" style="border: none;"> <?php echo $this->lang->line("Price");?> </th>
-                    <th class="action-td" style="border: none;"> <?php echo $this->lang->line("Option");?> </th>
-                  </tr>
+                 </tr>
                 </thead>
                 <tbody>
                 	<?php 
@@ -78,11 +75,6 @@
 						 $soldUsersstr=$soldUsersstr."  </select>  ";
 						 $soldUsersstr=base64_encode($soldUsersstr);
 						echo "<tr>";
-//                    	echo "<td style=\"width:5%; border: none;\" class=\"add-img-selector\"><div class=\"checkbox\">";
-//                        echo "<label>";
-//                        echo "  <input type=\"checkbox\">";
-//                        echo "</label>";
-//                      	echo "</div></td>";
                       	echo "<td style=\"width:20%;height:150px;padding:0px; margin: 0px; border: none;\"  class=\"add-image\">";
 //                       	echo  "<div class=\"col-sm-2 no-padding photobox\">";
 // 						echo "<div style=\"position:relative; height:75px; width: 100%; overlfow:hidden;\">";
@@ -101,46 +93,7 @@
 //                       	echo "</div>";
                       	
 //                       	echo "<a href=$viewItemPath><img class=\"thumbnail no-margin\" src=$imagePath alt=\"img\"></a>";
-                    	echo "<p class=\"price-hide\">$price</p>";
-						echo "<div class=\"action-hide\">";
-						$editPath=base_url().MY_PATH."newPost/showEditPost/".$messageID."?prevURL=".urlencode(current_url());
-						$sharePath=base_url().MY_PATH."messages/shareMyAds/".$messageID."/".$userID;
-						$deletePath=base_url().MY_PATH."messages/deleteMyAds/".$messageID."/".$userID;
-						
-						$rowCount=$rowCount+1;
-						$ctrlName1="AjaxLoad".$rowCount;
-						$errorctrlName1="ErrAjaxLoad".$rowCount;
-						$ctrlValue1="messageID".$rowCount;
-						$ctrlValue2="userID".$rowCount;
-						$clickLink="clickLink".$rowCount;
-						$shareLink=base_url().MY_PATH."viewItem/index/".$postID;
-						//echo "<p><a class=\"btn btn-primary btn-xs\" href=$editPath> <i class=\"fa fa-edit\"></i> ".$this->lang->line('Edit')." </a></p>";
-                        echo "<p> <a class=\"btn btn-info btn-xs\" href=\"#shareAds\"  data-toggle=\"modal\" shareLink='$shareLink'> <i class=\"fa fa-mail-forward\"></i>".$this->lang->line('Share')." </a></p>";
-                        echo "<p>";
-                        		
-                        echo " <div id='$ctrlName1' name='$ctrlName1' class='center'></div><div id='$errorctrlName1' name='$errorctrlName1' class='center'></div>";
-                    	echo "<input name='$ctrlValue1' id='$ctrlValue1' type='hidden' value='$messageID' />";
-                    	echo "<input name='$ctrlValue2' id='$ctrlValue2' type='hidden' value='$userID' />";
-                    	
-                    	echo "<div class=\"user-ads-action\"><a class=\"btn btn-danger btn-xs\"  href=\"#deleteAdsPopup\" data-toggle=\"modal\" id='$clickLink' data-id=\"$messageID\" data-userID=\"$userID\"> <i class=\" fa fa-trash\"></i> ".$this->lang->line('Delete')." </a></div></p>";
-                    	
-                        //echo "<a class=\"btn btn-danger btn-xs\"  href=\"javascript:deleteAds('$ctrlValue1','$ctrlValue2', '$ctrlName1', '$errorctrlName1)'\" id='$clickLink'> <i class=\" fa fa-trash\"></i> ".$this->lang->line('Delete')." </a></p>";
-                     	if($enableMarkSoldBtn)
-                        	echo "<p><div class=\"user-ads-action\"><a class=\"btn btn-info btn-xs\"  data-toggle=\"modal\"   href=\"#markSoldAds\"  data-id=\"$messageID\"  data-soldusers=\"$soldUsersstr\"> <i class=\"fa fa-mail-forward\"></i>".$this->lang->line('MarkSold')." </a></div></p>";
-                        
-                        echo "</div>";
-						echo "</td>";
-                      	echo "<td style=\"width:55%; border: none;\" class=\"ads-details-td\">";
-                    	echo "<div class=\"ads-details\">";
-                         echo "<h5><div class=\"add-title-girlstrade\">".$this->lang->line("lblTitle").$previewTitle."</div>".$previewDesc."<br/>".$preview;
-                          echo "<br/>Posted On: ". $createDate."<br/>Status: ".$status;
-                   	  echo "<br/>Interest persons count:  $NoOfSoldUsers";
-                        if(strcmp($status, "Rejected")==0){
-                        	echo "<br/>Reject Reason: ".$rejectReason." with ".$rejectSpecifiedReason;
-                        }
-                        echo "</div></h5></td>";
-                      	echo "<td style=\"width:10%; border: none;\" class=\"price-td\">$price</td>";
-						echo "<td style=\"width:10%; border: none;\" class=\"action-td\"><div>";
+                    	echo "<p class=\"price-td\">".$this->lang->line("Price")." :$price</p>";
 						$editPath=base_url().MY_PATH."newPost/showEditPost/".$messageID."?prevURL=".urlencode(current_url());
 						$sharePath=base_url().MY_PATH."messages/shareMyAds/".$messageID."/".$userID;
 						$deletePath=base_url().MY_PATH."messages/deleteMyAds/".$messageID."/".$userID;
@@ -171,6 +124,16 @@
                         	echo "<p><div class=\"user-ads-action\"><a class=\"btn btn-info btn-xs\"  data-toggle=\"modal\"   href=\"#confirmRepost\"  data-id=\"$messageID\"> <i class=\"fa fa-mail-forward\"></i>Repost</a></div></p>";
                         	
                         echo "</div></td>";
+                      	echo "<td style=\"width:55%; border: none;\" class=\"ads-details-td\">";
+                    	echo "<div class=\"ads-details\">";
+                         echo "<h5><div class=\"add-title-girlstrade\">".$this->lang->line("lblTitle").$previewTitle."</div>".$previewDesc."<br/>".$preview;
+                          echo "<br/>Posted On: ". $createDate."<br/>Status: ".$status;
+                   	  echo "<br/>Interest persons count:  $NoOfSoldUsers";
+                        if(strcmp($status, "Rejected")==0){
+                        	echo "<br/>Reject Reason: ".$rejectReason." with ".$rejectSpecifiedReason;
+                        }
+                        echo "</div></h5></td>";
+                      	
                   		echo "</tr>";
                   	}
             	}
