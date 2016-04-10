37ff432bfb83b5cdef9a259ac52cfea202735265
Raymond Chiu <rchiu5hk@yahoo.com.hk>
2016-04-10 10:38:45 +0800

    amend blog admin of action button

--- application/views/adminBlogContentHistory.php
+++ application/views/adminBlogContentHistory.php
@@ -27,15 +27,17 @@
                   		$description=$row->description;
                   		$clickLink="deleteBlog".$Num;
                 		echo "<tr>";
+                		echo "<td style=\"width:10%\" class=\"action-td\"><div>";
+                		echo "<a class=\"btn btn-danger btn-xs btn-120\"  href=\"#deleteBlogPopup\" data-toggle=\"modal\" id='$clickLink' data-id=\"$id\" data-title=\"$title\" data-description=\"$description\"> <i class=\" fa fa-trash\"></i> ".$this->lang->line('Delete')." </a></p>";
+                		 
+                		echo "</div>";
+                		echo "</td>";
                     	echo "<td style=\"width:60%\" class=\"ads-details-td\">";
                     	echo "<div class=\"ads-details\">";
                        echo "<h5>".$title."<br/>Description: ". $description."</h5>";
                       	 	echo "</div></td>";
-                      	 	echo "<td style=\"width:10%\" class=\"action-td\"><div>";
-                      	 		echo "<a class=\"btn btn-danger btn-xs btn-120\"  href=\"#deleteBlogPopup\" data-toggle=\"modal\" id='$clickLink' data-id=\"$id\" data-title=\"$title\" data-description=\"$description\"> <i class=\" fa fa-trash\"></i> ".$this->lang->line('Delete')." </a></p>";
-                    	
-                echo "</div>";
-                  echo "</td></tr>";
+                      	 
+                      	 	echo "</tr>";
 				}
                
             	}else{
